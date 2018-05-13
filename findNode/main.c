ee6b77f6625dae6134e0d7263222be8cb3a89dee
Jari Mutikainen <jari.mutikainen@welho.net>
2018-05-13 20:34:14 +0300

    Completed the 'find the node in the linked list' exercise.

--- findNode/main.c
+++ findNode/main.c
@@ -13,6 +13,8 @@ int main()
 {
     void getInputs(FILE* infile, int* n, int* k, node_t** head);
     void showList(node_t* head);
+    void findNode(node_t* head, int noi);
+    void releaseNodes(node_t** head);
     FILE* infile = fopen("input.txt","r");
     if(NULL == infile) {
         printf("Unable to open input.txt.\n");
@@ -22,11 +24,14 @@ int main()
     int noi; // Node Of Interest
     fscanf(infile,"%d",&T);
     node_t* top = NULL;     // The pointer to the 1st node of the linked list.
-    getInputs(infile,&n,&k,&top);
-    showList(top);
-    if(n % k == 0) noi = n / k;
-    else noi = n / k + 1; // The poor man's ceil function is here.
-
+    for(int t=0;t<T;t++) {
+        getInputs(infile,&n,&k,&top);
+        showList(top);
+        if(n % k == 0) noi = n / k;
+        else noi = n / k + 1; // The poor man's ceil function is here.
+        findNode(top,noi);
+        releaseNodes(&top);
+    }
 
 
     fclose(infile);
@@ -67,6 +72,7 @@ void getInputs(FILE* infile, int* n, int* k, node_t** head) {
 void showList(node_t* head) {
     // Prints the contents of the linked list
     node_t* p = head;
+    printf("\n");
     if(NULL == p) {
         printf("The linked list is empty.\n");
     } else {
@@ -77,5 +83,43 @@ void showList(node_t* head) {
         // p is pointing at the last node of the list now.
         printf("Node %d, value %d\n",p->indx,p->val);
     }
+    return;
+}
+//------------------------------ findNode ------------------------------  
+void findNode(node_t* head, int noi) {
+    // Finds and prints out the node of interest.
+    node_t* p = head;
+    if(NULL == p) {
+        printf("The list is empty. No element to print.\n");
+        return;
+    }
+    while(p->next != NULL) {
+        if(p->indx == noi) {
+            printf("\nNode of interest: %d, val = %d.\n",p->indx,p->val);
+            return;
+        }
+        p = p->next;
+    }
+    // p is pointing at the last node now.
+    if(p->indx == noi) {
+        printf("\nNode of interest: %d, val = %d.\n",p->indx,p->val);
+    }
+    return;
+}
+//------------------------------ releaseNodes ------------------------------  
+void releaseNodes(node_t** head) {
+    // Releases all the nodes of the linked list and sets the top
+    // pointer in main to NULL.
+    node_t *p = *head, *temp = NULL;
+    while(p->next != NULL) {
+        //printf("Releasing Node %d, val = %d.\n",p->indx,p->val);
+        temp = p->next;
+        free(p);
+        p = temp;
+    }
+    // p is pointing at the last node now
+    //printf("Releasing Node %d, val = %d.\n",p->indx,p->val);
+    free(p);
+    *head = NULL;
     return;
 }
