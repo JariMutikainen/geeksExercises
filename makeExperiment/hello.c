3bc243c697081e8b89cc45ffe45e732cc3697059
Jari Mutikainen <jari.mutikainen@welho.net>
2018-05-19 19:39:43 +0300

    Completed the makefile experiment in the Cygwin environment.

---
+++ makeExperiment/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+    printf("Hello all you makefile users!\n");
+    printf("Insert a number: ");
+    int num;
+    scanf("%d",&num);
+    printf("You entered %d.\n",num);
+    return 0;
+}
+
